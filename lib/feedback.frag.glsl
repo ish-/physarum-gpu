65e20ff3f06bd4943efb1740beb3f7df99fbd831
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2023-07-06 23:27:14 +0400

    before Feedback refact

--- lib/feedback.frag.glsl
+++ lib/feedback.frag.glsl
@@ -2,7 +2,7 @@ uniform float damp;
 
 uniform sampler2D tOld;
 uniform sampler2D tNew;
-uniform int frame;
+uniform int thisFrame;
 uniform vec4 resetColor;
 
 varying vec2 vUv;
@@ -10,32 +10,26 @@ varying vec2 vUv;
 // main
 
 vec4 when_gt( vec4 x, float y ) {
-
   return max( sign( x - y ), 0.0 );
-
 }
 
 void main() {
-
-  vec4 texelOld;
-  if (frame == -1) {
-    // texelOld = resetColor;
+  if (thisFrame == -1) {
     gl_FragColor = resetColor;
     return;
-  } else {
-    texelOld = texture2D( tOld, vUv );
   }
-  vec4 texelNew = texture2D( tNew, vUv );
 
-  // #ifdef __main
-  // vec4 res = _main(texelOld, texelNew);
-  // #endif
+  #ifdef __main
+    _main();
+  #endif
 
-  // #ifdef damping
-  // texelOld *= damp * when_gt( texelOld, 0.1 );
-  // #endif
-
-
-  gl_FragColor = texelNew* .01 + texelOld;
+  #ifndef __main
+    vec4 texelOld = texture2D( tOld, vUv );
+    vec4 texelNew = texture2D( tNew, vUv );
+    gl_FragColor = texelNew* .01 + texelOld;
+      #ifdef damping
+        texelOld *= damp * when_gt( texelOld, 0.1 );
+      #endif
+  #endif
 
 }
--- scenes/r_pts-insts/index.js
+++ scenes/r_pts-insts/index.js
@@ -13,93 +13,74 @@ import {
   MeshBasicMaterial,
   Mesh,
 } from 'three';
-import Quad from '/lib/Quad.js';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js'
-import { GlitchPass } from 'three/addons/postprocessing/GlitchPass.js'
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js'
-import Feedback from '/lib/Feedback.js';
-import defaultUvVert from '/shaders/defaultUv.vert.glsl?raw';
-import opacityFrag from '/shaders/opacity.frag.glsl?raw';
-import { matUniformGui } from '/lib/gui.js';
-// import RenderTargetInspector from '/lib/RenderTargetInspector.js';
-import mxNoiseGlsl from '/lib/mx-noise.glsl?raw';
-import tmpFrag from './tmp.frag.glsl?raw';
-import { GUI } from 'dat.gui';
+
+import { GuiUniforms } from '/lib/gui';
+import Feedback from '/lib/Feedback';
 import QuadFrame from '/lib/QuadFrame';
 
+import defaultUvVert from '/shaders/defaultUv.vert.glsl?raw';
+import opacityFrag from '/shaders/opacity.frag.glsl?raw';
+import mxNoiseGlsl from '/lib/mx-noise.glsl?raw';
+import tmpFrag from './tmp.frag.glsl?raw';
 
 import createInstances from './instances.js';
 
-function go ({ scene, renderer, camera, width, height }) {
-  const testTex = new TextureLoader().load('/assets/boxMap.jpg');
-  const noiseFragGlsl = tmpFrag.replace('// <functions>', mxNoiseGlsl);
+const testTex = new TextureLoader().load('/assets/boxMap.jpg');
+const noiseFragGlsl = tmpFrag.replace('// <functions>', mxNoiseGlsl);
 
-  const amountSq = 10;
+const amountSq = 10;
 
-  // velocity
-  // const velRT = new WebGLRenderTarget(amountSq, amountSq, {
-  //   type: FloatType, minFilter: NearestFilter, magFilter: NearestFilter });
-  const noiseMat = new ShaderMaterial({
-    uniforms: {
-      opacity: { value: .6 },
-      time: { value: 0 },
-      tSource: { value: testTex },
+const noiseMat = new ShaderMaterial({
+  uniforms: GuiUniforms('noises', {
+    octaves: 2,
+    lacunarity: .5,
+    diminish: .5,
+    scale: 1,
+  }).noGui({
+    time: 0,
+  }),
+  vertexShader: defaultUvVert,
+  fragmentShader: noiseFragGlsl,
+});
 
-      octaves: { value: 2 },
-      lacunarity: { value: .5 },
-      diminish: { value: .5 },
-      scale: { value: 1 },
-    },
-    vertexShader: defaultUvVert,
-    fragmentShader: noiseFragGlsl,
-  });
+const velFrame = new QuadFrame({
+  ...QuadFrame.computeOpts,
+  size: amountSq,
+  material: noiseMat,
+});
 
-  const velFrame = new QuadFrame({
-    size: amountSq,
-    material: noiseMat,
-    type: FloatType, minFilter: NearestFilter, magFilter: NearestFilter })
+// POS
 
-  const gui = new GUI();
-  const fold = gui.addFolder('Cube');
-  matUniformGui(noiseMat, fold)
-    .add('octaves').add('lacunarity').add('diminish').add('scale');
-  fold.open();
+const posFB = new Feedback({ size: amountSq,
+  uniforms: {
+    tNew: { value: velFrame.texture },
+  },
+  shader: {
+    damping: false,
+    // process: `
 
-  // const velQuad = new Quad(noiseMat);
+    // `
+  }
+});
 
-  // POS
+window.addEventListener('keydown', e => {
+  if (e.key === 'r')
+    posFB.reset();
+})
 
-  const posFB = new Feedback({ size: amountSq,
-    uniforms: {
-      tNew: { value: velFrame.texture },
-    },
-    shader: {
-      damping: false,
-    }
-  });
+// INSTANCES
+const mesh = createInstances(amountSq, posFB.texture);
+sketch.scene.add(mesh);
+sketch.scene.position.z = -2;
 
-  // INSTANCES
-  const mesh = createInstances(amountSq, posFB.texture);
-  scene.add(mesh);
-  scene.position.z = -2;
 
-  return function ({ now, elapsed, delta, render }) {
-    noiseMat.uniforms.time.value = elapsed/2;
-    velFrame.render();
-    posFB.render();
+sketch.startRaf(({ now, elapsed, delta }) => {
+  noiseMat.uniforms.time.value = elapsed/2;
+  velFrame.render();
+  posFB.render();
 
-    render()
-    // debug({ texture: velFrame.texture })
+  sketch.render();
+  // debug({ texture: velFrame.texture })
+});
 
-    return { renderSketch: false }
-  };
-}
-
-sketch.startRaf(go({
-  scene: sketch.scene,
-  renderer: sketch.renderer,
-  camera: sketch.camera,
-  width: sketch.W,
-  height: sketch.H,
-}))
 export default {};
