48bdb991f465eaf45aad8fc792fd5fb8cbcabcb6
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2023-07-10 20:48:18 +0400

    try superframe = bad decl

--- scenes/r_test/index.js
+++ scenes/r_test/index.js
@@ -2,6 +2,7 @@ import {
   BoxGeometry,
   ShaderMaterial,
   OrthographicCamera,
+  NearestFilter
 } from 'three';
 
 import sketch, { debug } from '/lib/Sketch';
@@ -10,6 +11,7 @@ import NoiseFrame from '/lib/NoiseFrame';
 import defaultUvVertGlsl from '/shaders/defaultUv.vert.glsl';
 import createInstances from '../r_pts-insts/instances';
 import { gui } from '/lib/gui';
+import SuperFrame from '/lib/SuperFrame';
 
 const COUNT_SQ = 10;
 
@@ -30,20 +32,42 @@ const uvFragGlsl = `
 // });
 // posFrame.render();
 
-const noiseFrame = new NoiseFrame({
-  size: 10,
-  range: [0, 1],
-  uniforms: { speed: .01 },
-});
+// const noiseFrame = new NoiseFrame({
+//   size: 10,
+//   range: [0, 1],
+//   uniforms: { speed: .01 },
+// });
 
-const instances = createInstances(COUNT_SQ, noiseFrame.texture);
-instances.material.uniforms.pointSize.value = 50;
-sketch.scene.add(instances);
+// const instances = createInstances(COUNT_SQ, noiseFrame.texture);
+// instances.material.uniforms.pointSize.value = 50;
+// sketch.scene.add(instances);
+
+// const f = new SuperFrame({
+//   // size: 10,
+//   // filter: NearestFilter,
+//   guiUniforms: {
+//     edgeSmooth: [.005, 0, 0.1, 0.001],
+//   },
+//   fragmentShader: `
+//     float sdBox( in vec2 p, in vec2 b ) {
+//       vec2 d = abs(p)-b;
+//       return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+//     }
+//     void main () {
+//       float sizeMod = sin(time.x) / 4.;
+//       float box = smoothstep(edgeSmooth * sizeMod + .25, 0., sdBox((vUv - vec2(.5)), vec2(.25 + sizeMod)));
+//       gl_FragColor = vec4(box, box, 0., 1.);
+//     }
+//   `,
+// });
+
 
 sketch.camera = new OrthographicCamera(0, 1, 1, 0, 0, 1);
 
 sketch.startRaf(({ elapsed }) => {
-  noiseFrame.material.uniforms.time.value = elapsed;
-  noiseFrame.render();
-  sketch.render();
+  // f.render();
+  // debug(f);
+  // noiseFrame.material.uniforms.time.value = elapsed;
+  // noiseFrame.render();
+  // sketch.render();
 });
