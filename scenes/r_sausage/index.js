f385aa9b16210d9ba34de22e23c2b648c9cc21db
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2023-08-17 02:47:53 +0400

    prod ready for sausage

--- scenes/r_sausage/index.js
+++ scenes/r_sausage/index.js
@@ -15,28 +15,20 @@ import {
   Vector3,
   Color,
 } from 'three';
-import { gui, Presets } from '/lib/gui';
-import { insertAfter, callnpass, rand, toFixed, modifyShader, modifyMatShader, hash2 } from '/lib/utils';
-import InstancingTransforms from '/lib/InstancingTransforms';
-import { OrbitControls } from 'three/addons/controls/OrbitControls';
 
+import InstancingTransforms from '/lib/InstancingTransforms';
 import QuadFrame from '/lib/QuadFrame';
 import defaultUvVertGlsl from '/shaders/defaultUv.vert.glsl?raw';
-
 import pulseFragModGlsl from './pulse.frag.mod.glsl?raw';
 import gradientGlsl from './gradient.frag.glsl?raw';
+import circlesData from './circles.half.json';
 
-import _circlesData from './circles.json';
+import { insertAfter, callnpass, rand, toFixed, modifyShader, modifyMatShader, hash2 } from '/lib/utils';
 
-sketch
-  .initGui({ reset: false })
-  .initKeys();
 sketch.dpi = 1;
-// sketch.renderer.domElement.style.filter = 'blur(.5px)';
-const circlesData = _circlesData.reduce((m, d, i) => {
-  if (!(i % 2)) m.push(d);
-  return m;
-}, []);
+// sketch.initKeys();
+
+const COUNT = circlesData.length;
 
 const PARS = {
   size: 1,
@@ -53,53 +45,6 @@ const PARS = {
   pulse,
   pulsePlay: true,
 };
-const INFO = {
-  camRot: '',
-  camPos: '',
-}
-
-function toggleOrigin (v = PARS.origin) { console.log(v);sketch.scene[v?'add':'remove'](testMesh) }
-gui.add(PARS, 'origin').onChange(toggleOrigin);
-gui.add(PARS, 'size', .0001, 10, .001).onChange(setScale);
-gui.add(PARS, 'rotateStep', 0, 100, .1).onChange(setRotation);
-gui.add(PARS, 'rotateSpeed', 0, 50, .001).onChange(v => v === 0 && setRotation());
-gui.addColor(PARS.colorStart, 'value').name('colorStart')
-  .onChange(v => albedo.render());
-gui.addColor(PARS.colorStop, 'value').name('colorStop')
-  .onChange(v => albedo.render());
-gui.add(PARS.pulseLerp, 'value', 0, 1, .001).name('pulseLerp').listen();
-gui.add(PARS.pulseWidth, 'value', 0, 2, .001).name('pulseWidth').listen();
-gui.add(PARS, 'pulseDuration', 0.001, 10, .01);
-gui.add(PARS, 'pulse');
-gui.add(PARS, 'pulseInterval', 0.1, 30, .01).onChange(pulseInterval);
-gui.add(PARS, 'pulseIntervalRand', 0, 10, .1);
-gui.add(PARS, 'pulsePlay').onChange(v => v ? pulseInterval() : clearTimeout(pulseIi));
-function animatePulse ({ delta }) {
-  PARS.pulseLerp.value += delta / PARS.pulseDuration;
-  if (PARS.pulseLerp.value > 1.) {
-    sketch.off('render', animatePulse);
-    PARS.pulseLerp.value = 0;
-  }
-}
-gui.add(INFO, 'camRot').listen();
-gui.add(INFO, 'camPos').listen();
-
-let pulseIi;
-function pulseInterval (
-  time = PARS.pulseInterval + PARS.pulseDuration + rand(-1)*PARS.pulseIntervalRand
-) {
-  clearTimeout(pulseIi);
-
-  pulseIi = setTimeout(() => {
-    console.log('pulseInterval.pulse')
-    PARS.pulse();
-    pulseInterval();
-  }, time * 1e3);
-}
-pulseIi = pulseInterval();
-
-const COUNT = circlesData.length;
-console.log({COUNT})
 
 const circleGeo = new CircleGeometry(.4, 64);
 const offsetAttr = new Float32Array(COUNT * 3);
@@ -178,14 +123,40 @@ toggleOrigin();
 
 sketch.scene.background = albedo.texture;
 
-const orbit = new OrbitControls(sketch.camera, sketch.renderer.domElement);
-orbit.listenToKeyEvents( window );
 
 handleResize();
 sketch.on('resize', handleResize);
 
-sketch.initStats();
+// sketch.initGui({ reset: false }).then(gui => {
+//   gui.add(PARS, 'origin').onChange(toggleOrigin);
+//   gui.add(PARS, 'size', .0001, 10, .001).onChange(setScale);
+//   gui.add(PARS, 'rotateStep', 0, 100, .1).onChange(setRotation);
+//   gui.add(PARS, 'rotateSpeed', 0, 50, .001).onChange(v => v === 0 && setRotation());
+//   gui.addColor(PARS.colorStart, 'value').name('colorStart')
+//     .onChange(v => albedo.render());
+//   gui.addColor(PARS.colorStop, 'value').name('colorStop')
+//     .onChange(v => albedo.render());
+//   gui.add(PARS.pulseLerp, 'value', 0, 1, .001).name('pulseLerp').listen();
+//   gui.add(PARS.pulseWidth, 'value', 0, 2, .001).name('pulseWidth').listen();
+//   gui.add(PARS, 'pulseDuration', 0.001, 10, .01);
+//   gui.add(PARS, 'pulse');
+//   gui.add(PARS, 'pulseInterval', 0.1, 30, .01).onChange(pulseInterval);
+//   gui.add(PARS, 'pulseIntervalRand', 0, 10, .1);
+//   gui.add(PARS, 'pulsePlay').onChange(v => v ? pulseInterval() : clearTimeout(pulseIi));
+// });
+
+// const INFO = {
+//   camRot: '',
+//   camPos: '',
+// }
+// gui.add(INFO, 'camRot').listen();
+// gui.add(INFO, 'camPos').listen();
+
+// sketch.initOrbitCtrl();
+// sketch.initStats();
+
 pulse();
+let pulseIi = setTimeout(pulseInterval);
 
 const camPivot = new Object3D(0, 0, 2);
 sketch.scene.add(camPivot);
@@ -214,16 +185,24 @@ sketch.startRaf(({ now, elapsed, delta }) => {
     camera.updateMatrix();
   })();
 
-  (() => {
-    const { camera: c } = sketch;
-    const r = c.rotation;
-    INFO.camRot = `${toFixed(r.x)},${toFixed(r.y)},${toFixed(r.z)}`;
-    const p = c.position;
-    INFO.camPos = `${toFixed(p.x)},${toFixed(p.y)},${toFixed(p.z)},${toFixed(c.zoom)}`;
-  })()
+  // (() => {
+  //   const { camera: c } = sketch;
+  //   const r = c.rotation;
+  //   INFO.camRot = `${toFixed(r.x)},${toFixed(r.y)},${toFixed(r.z)}`;
+  //   const p = c.position;
+  //   INFO.camPos = `${toFixed(p.x)},${toFixed(p.y)},${toFixed(p.z)},${toFixed(c.zoom)}`;
+  // })()
 
   sketch.render();
 });
+
+function animatePulse ({ delta }) {
+  PARS.pulseLerp.value += delta / PARS.pulseDuration;
+  if (PARS.pulseLerp.value > 1.) {
+    sketch.off('render', animatePulse);
+    PARS.pulseLerp.value = 0;
+  }
+}
 
 function pulse () {
   PARS.pulseLerp.value = 0;
@@ -248,4 +227,17 @@ function handleResize ({ size } = sketch) {
   sketch.camera.right = aspect;
   sketch.camera.left = -aspect;
   sketch.camera.updateProjectionMatrix();
+}
+
+function toggleOrigin (v = PARS.origin) { sketch.scene[v?'add':'remove'](testMesh) }
+function pulseInterval (
+  time = PARS.pulseInterval + PARS.pulseDuration + rand(-1)*PARS.pulseIntervalRand
+) {
+  clearTimeout(pulseIi);
+
+  pulseIi = setTimeout(() => {
+    console.log('pulseInterval.pulse')
+    PARS.pulse();
+    pulseInterval();
+  }, time * 1e3);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 // import { resolve } from 'path';
 // const glsl = require('vite-plugin-glsl');
 import glslify from  'vite-plugin-glslify';
-import mkcert from 'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert';
+
+import dotenv from 'dotenv';
+
+const env = dotenv.config().parsed;
 
 // import { defineConfig } from 'vite';
 
 export default {
   server: { https: true },
+
+  define: {
+    DEBUG: `"${env.DEBUG}"`,
+    VITE_AZAZA: "'yeah!'",
+  },
+
+  // build: {
+  //   rollupOptions: {
+  //     treeshake: {
+  //       annotations: false,
+  //       moduleSideEffects: 'no-external',
+  //       // preset: 'smallest',
+  //     },
+  //   },
+  // },
 
   plugins: [
     // glsl()
